df436eb605f30d7072f1f4064b901b784c663daf
kouevi ekoue <kouevi.ekoue@student.vinci.be>
2025-01-17 17:30:17 +0100

    Refactor function

--- passwordChecker.js
+++ passwordChecker.js
@@ -2,6 +2,7 @@ function validatePassword(password) {
     if (password.length < 8) return false;
     if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) return false;
     if (!/\d/.test(password)) return false;
+    if (/ipl/i.test(password)) return false;
     return true;
 }
 
