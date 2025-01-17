ca7c435507ada851b3c6cabb9c66d1315a4871ee
kouevi ekoue <kouevi.ekoue@student.vinci.be>
2025-01-17 17:26:59 +0100

    Add test

--- spec/passwordChecker.spec.js
+++ spec/passwordChecker.spec.js
@@ -4,5 +4,10 @@ describe('Validation de mot de passe', () => {
     it('doit valider un mot de passe correct', () => {
         expect(validatePassword('Passw0rd!')).toBe(true);
     });
+
+    it('doit invalider un mot de passe trop court', () => {
+        expect(validatePassword('P@ss1')).toBe(false);
+    });
+    
     
 });
